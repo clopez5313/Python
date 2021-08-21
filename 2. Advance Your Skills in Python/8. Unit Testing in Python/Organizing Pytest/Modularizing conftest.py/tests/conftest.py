03e0299a9aeb73fdfdab3f11a664bcefe9f42823
Carlos Lopez <clopez5313@gmail.com>
2021-08-21 16:45:44 -0500

    Advance Your Skills in Python #8

---
+++ 2. Advance Your Skills in Python/8. Unit Testing in Python/Organizing Pytest/Modularizing conftest.py/tests/conftest.py
@@ -0,0 +1,13 @@
+# from utility.cities import city_list_location #pylint:disable=unused-import
+# from utility.data_processing import process_data #pylint:disable=unused-import
+
+"""
+This file is for walkthrough purposes only.
+Normally, conftest.py file should be located directly under the "tests/" folder.
+"""
+
+
+pytest_plugins = [
+    "tests.utility.cities",
+    "tests.utility.data_processing"
+]
---
+++ 2. Advance Your Skills in Python/8. Unit Testing in Python/Test-driven Development and Pytest Features/Challenge/tests/test.py
@@ -0,0 +1,19 @@
+from scripts.chp2.video4.mapmaker_challenge import Point
+import pytest
+
+
+def test_make_one_point():
+    p1 = Point("Dakar", 14.7167, 17.4677)
+    assert p1.get_lat_long() == (14.7167, 17.4677)
+
+
+def test_invalid_point_generation():
+    with pytest.raises(ValueError) as exp:
+        Point("Senegal", 99.6937, -189.44406)
+    assert str(exp.value) == "Invalid latitude, longitude combination."
+
+
+def test_invalid_city():
+    with pytest.raises(TypeError) as exp:
+        Point(1, 14.7167, 17.4677)
+    assert str(exp.value) == "City names should be Strings."
